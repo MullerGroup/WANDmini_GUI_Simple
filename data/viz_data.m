24ea95fcc6aca59f22d80ad53e8d00200b064097
Andy Zhou <andy@andyz-macbook16.attlocal.net>
2021-02-10 20:29:40 -0800

    updated visualization script

--- data/viz_data.m
+++ data/viz_data.m
@@ -16,15 +16,7 @@ for i = 1:length(crcIdx)
     clean(crcIdx,:) = clean(crcIdx-1,:);
 end
 
-emg = clean(:,1:64);
-temp = uint16(clean(:,65:67));
-acc = zeros(size(temp));
-for ch = 1:3
-    acc(:,ch) = double(typecast(temp(:,ch),'int16'));
-end
+data = clean(:,1:64);
 
 figure
-subplot(2,1,1)
-plot(emg)
-subplot(2,1,2)
-plot(acc)
+plot(data)
